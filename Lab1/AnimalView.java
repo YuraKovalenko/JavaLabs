68dadca143ee5e707d23a2889622876c632d0930
YuraKovalenko <57789851+YuraKovalenko@users.noreply.github.com>
2021-04-12 15:18:45 +0300

    Update AnimalView.java

--- Lab1/AnimalView.java
+++ Lab1/AnimalView.java
@@ -1,36 +1,35 @@
 package com.java.View;
 
+import com.java.Model.Animal;
+
 public class AnimalView {
-    public void messageForFuncMorThanAge(){
-        System.out.println("Немає тварин старше заданого віку");
-    }
-    public void messageForFuncEqualsFamily(){
-        System.out.println("Немає тварин заданого сімейства");
-    }
-    public void messageForFuncEqualSpeciesAndColor(){
-        System.out.println("Немає тварин заданого виду та окрасу");
-    }
-    public void printMenu(){
+    public static final String ERR_NUMB_FOR_MENU = "Помилка! Введіть потрібне значення ще раз";
+    public static final String ENTER_AGE = "Введіть вік ";
+    public static final String RESULT = "Пошук не дав результату";
+    public static final String ENTER_FAM = "Введіть назву сімейства ";
+    public static final String ENTER_SP_AND_COL = "Введіть вид та окрас ";
+
+    public void printMenu() {
         System.out.println("1 щоб отримати весь список");
         System.out.println("2 щоб отримати список тварин старше заданого віку");
         System.out.println("3 щоб отримати список тварин заданого сімейства");
         System.out.println("4 щоб отримати список тварин заданого виду і окрасу");
         System.out.println("5 завершити роботу");
-        System.out.print("Введіть значення для роботи: ");
+        System.out.println("Введіть значення для роботи: ");
     }
-    public void errorMessage(){
-        System.out.println("Помилка! Введіть потрібне значення ще раз");
+
+    public void printMessage(String message) {
+        System.out.println(message);
     }
-    public void messageForCase2(){
-        System.out.println("Введіть вік");
-    }
-    public void messageForCase3(){
-        System.out.println("Введіть назву сімейства");
-    }
-    public void messageForCase4(){
-        System.out.println("Введіть вид та назву сімейства");
-    }
-    public void message(){
-        System.out.print("Введіть значення для роботи: ");
+
+    public void printArray(Animal[] animal) {
+        Animal[] temp = new Animal[animal.length];
+        System.arraycopy(animal, 0, temp, 0, animal.length);
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println(temp[i]);
+        }
+        if (temp.length == 0) {
+            System.out.println(RESULT);
+        }
     }
 }
