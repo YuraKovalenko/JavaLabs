74225b500f5d0c9a97c6b3978abe15ebdc87189f
YuraKovalenko <57789851+YuraKovalenko@users.noreply.github.com>
2021-04-12 15:19:11 +0300

    Update Initialisation.java

--- Lab1/Initialisation.java
+++ Lab1/Initialisation.java
@@ -1,18 +1,17 @@
-package com.java.Initialisation;
-
-import com.java.Model.Animal;
+package com.java.Model;
 
 public class Initialisation {
-    public static void initialization(Animal[] animal){
-        animal[0]=new Animal("q0","w","cat","r","sp1","y",10,"red");
-        animal[1]=new Animal("q1","w","dog","r","sp2","y",10,"red");
-        animal[2]=new Animal("q2","w","cat","r","sp3","y",17,"blue");
-        animal[3]=new Animal("q3","w","cow","r","sp3","y",12,"green");
-        animal[4]=new Animal("q4","w","cat","r","sp2","y",20,"red");
-        animal[5]=new Animal("q5","w","cow","r","sp1","y",18,"green");
-        animal[6]=new Animal("q6","w","cat","r","sp1","y",10,"red");
-        animal[7]=new Animal("q7","w","dog","r","sp3","y",15,"green");
-        animal[8]=new Animal("q8","w","cow","r","sp1","y",15,"yellow");
-        animal[9]=new Animal("q9","w","dog","r","sp2","y",17,"blue");
+    public static void initialization(Animal[] animal) {
+        animal[0] = new Animal("q0", "w", "cat", "r", "sp1", "y", 10, "red");
+        animal[1] = new Animal("q1", "w", "dog", "r", "sp2", "y", 10, "red");
+        animal[2] = new Animal("q2", "w", "cat", "r", "sp3", "y", 17, "blue");
+        animal[3] = new Animal("q3", "w", "cow", "r", "sp3", "y", 12, "green");
+        animal[4] = new Animal("q4", "w", "cat", "r", "sp2", "y", 20, "red");
+        animal[5] = new Animal("q5", "w", "cow", "r", "sp1", "y", 18, "green");
+        animal[6] = new Animal("q6", "w", "cat", "r", "sp1", "y", 10, "red");
+        animal[7] = new Animal("q7", "w", "dog", "r", "sp3", "y", 15, "green");
+        animal[8] = new Animal("q8", "w", "cow", "r", "sp1", "y", 15, "yellow");
+        animal[9] = new Animal("q9", "w", "dog", "r", "sp2", "y", 17, "blue");
     }
+
 }
