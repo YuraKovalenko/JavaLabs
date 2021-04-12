7827b09d4bf586cfba824279eff9fcd9d2a8dac0
YuraKovalenko <57789851+YuraKovalenko@users.noreply.github.com>
2021-04-12 15:20:08 +0300

    Update Methods.java

--- Lab1/Methods.java
+++ Lab1/Methods.java
@@ -1,71 +1,58 @@
-package com.java.Methods;
-
-import com.java.Model.Animal;
-import com.java.Initialisation.Initialisation;
-import com.java.View.AnimalView;
+package com.java.Model;
 
 public class Methods {
-    Animal[] animal = new Animal[10];
-    AnimalView animalView = new AnimalView();
+    private Animal[] animal = new Animal[10];
 
-    public void create(){
+    public Methods() {
         Initialisation.initialization(animal);
     }
 
-    public void printAllAnimals(){
-        for(int i = 0; i < animal.length; i++){
-            System.out.println(animal[i]);
+    public Animal[] funcMorThanAge(int morThanAge) {
+        Animal[] temp = new Animal[10];
+        int index = 0;
+        for (int i = 0; i < animal.length; i++) {
+            if (animal[i].getAge() > morThanAge) {
+                System.arraycopy(animal, i, temp, index, 1);
+                index++;
+            }
         }
+        Animal[] out = new Animal[index];
+        System.arraycopy(temp, 0, out, 0, index);
+        return out;
     }
 
-    public void funcMorThanAge(int age){
-        if(age>20){
-            animalView.messageForFuncMorThanAge();
-        }
-        else {
-            for(int i = 0; i < animal.length; i++){
-                if(animal[i].getAge() > age){
-                    System.out.println(animal[i]);
-                }
+    public Animal[] funcForEqualFamily(String fam) {
+        Animal[] temp = new Animal[10];
+        int index = 0;
+        for (int i = 0; i < animal.length; i++) {
+            if (animal[i].getFamily().equals(fam)) {
+                System.arraycopy(animal, i, temp, index, 1);
+                index++;
             }
         }
+        Animal[] out = new Animal[index];
+        System.arraycopy(temp, 0, out, 0, index);
+        return out;
     }
 
-    public void equalFamily(String fam){
-        if("cat".equals(fam) || "dog".equals(fam) || "cow".equals(fam)){
-            for(int i = 0; i < animal.length; i++){
-                if (animal[i].getFamily().equals(fam)){
-                    System.out.println(animal[i]);
-                }
+    public Animal[] funcForEqualSpeciesAndColor(String species, String color) {
+        Animal[] temp = new Animal[10];
+        int index = 0;
+        for (int i = 0; i < animal.length; i++) {
+            if (animal[i].getSpecies().equals(species) && animal[i].getColor().equals(color)) {
+                System.arraycopy(animal, i, temp, index, 1);
+                index++;
             }
         }
-        else {
-            animalView.messageForFuncEqualsFamily();
-        }
+        Animal[] out = new Animal[index];
+        System.arraycopy(temp, 0, out, 0, index);
+        return out;
     }
 
-    public void equalSpeciesAndColor(String species, String color){
-        String[]str = new String[10];
-        int count = 0;
-        if ("sp1".equals(species) || "sp2".equals(species) || "sp3".equals(species) &&
-        "blue".equals(color) || "red".equals(color) || "green".equals(color) || "yellow".equals(color)){
-            for(int i = 0; i < animal.length; i++){
-                if(animal[i].getSpecies().equals(species) && animal[i].getColor().equals(color)){
-                    str[i]=animal[i].getSpecies();
-                    System.out.println(animal[i]);
-                }
-            }
-            for (int i = 0;i<10;i++){
-                if (str[i]!=null){
-                    count++;
-                }
-            }
-            if (count==0){
-                animalView.messageForFuncEqualSpeciesAndColor();
-            }
-        }
-        else {
-            animalView.messageForFuncEqualSpeciesAndColor();
-        }
+    public Animal[] getAnimal() {
+        Animal[] temp = new Animal[10];
+        System.arraycopy(animal, 0, temp, 0, 10);
+        return temp;
     }
+
 }
